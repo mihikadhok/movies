5acc1d3aacc148bf7865ab364d6720b3fa0d920e
mihikadhok <106306886+mihikadhok@users.noreply.github.com>
2022-05-27 11:20:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("percyJackson").onclick = function () {
+        location.href = "hollywood/percyJackson.html";
+    };
+
+document.getElementById("skyHigh").onclick = function () {
+        location.href = "hollywood/skyHigh.html";
+    };
