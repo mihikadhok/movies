1df80e30f6b7d27fe3f973614b100f95b219d718
Mihika Dhok <Mihika.Dhok@aurumproptech.in>
2022-05-31 10:45:51 +0530

    revamped

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+
+// hollywood
+
 document.getElementById("percyJackson").onclick = function () {
         location.href = "hollywood/percyJackson.html";
     };
 
 document.getElementById("skyHigh").onclick = function () {
         location.href = "hollywood/skyHigh.html";
+    };
+
+document.getElementById("aladdin").onclick = function () {
+        location.href = "hollywood/aladdin.html";
+    };
+
+    // bollywood
+
+document.getElementById("blueUmbrella").onclick = function () {
+        location.href = "bollywood/blueUmbrella.html";
     };
